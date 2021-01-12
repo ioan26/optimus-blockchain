2682b4338a59b11d6f16a38d3d206019f1ff0506
ioan26 <ioan.oara@gmail.com>
2021-01-12 16:38:38 +0100

    hash generator improved: testing file

--- hash_gen_test.cpp
+++ hash_gen_test.cpp
@@ -1,31 +1,37 @@
-#include "openssl/sha.h"
-#include <cstring>
+#include <iostream>
 #include <chrono>
+#include <iomanip>
+#include <sstream>
+#include <string>
+
 using namespace std;
+#include "openssl/sha.h"
 
-void sha256_hash_string (unsigned char hash[SHA256_DIGEST_LENGTH], char outputBuffer[65])
+string sha256(const string str)
 {
-    int i = 0;
-
-    for(i = 0; i < SHA256_DIGEST_LENGTH; i++)
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, str.c_str(), str.size());
+    SHA256_Final(hash, &sha256);
+    stringstream ss;
+    for(int i = 0; i < SHA256_DIGEST_LENGTH; i++)
     {
-        sprintf(outputBuffer + (i * 2), "%02x", hash[i]);
+        ss << hex << setw(2) << setfill('0') << (int)hash[i];
     }
-
-    outputBuffer[64] = 0;
+    return ss.str();
 }
 
 int main(int argc, char const *argv[]) {
-  /* code */
   using namespace std::chrono;
 
-  system_clock::time_point today = system_clock::now();
-  auto duration = today.time_since_epoch();
-  auto millis = duration_cast<milliseconds>(duration).count();
 
+  const auto p1 = std::chrono::system_clock::now();
+  const unsigned long int nano = std::chrono::duration_cast<std::chrono::nanoseconds>(p1.time_since_epoch()).count();
 
-  static unsigned char buffer[65];
-  sha256_hash_string(ctime(&millis), buffer);
+  string perfect = to_string(nano);
+
+  cout << sha256(perfect) << endl;
 
   return 0;
 }
